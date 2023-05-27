245d35b413051809d75b109e19414f307893c356
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-05-26 22:43:13 -0300

    config migrations, data-source, initializebd

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,29 +4,40 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm'
+import { AppDataSource } from 'src/data-source';
 
-const app = express();
 
-app.use(cors());
-app.use(express.json());
+AppDataSource.initialize().then(() => {
+  const app = express();
 
-app.use(routes);
+  app.use(cors());
+  app.use(express.json());
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
+  app.use(routes);
+
+  app.use(
+    (
+      error: Error,
+      request: Request,
+      response: Response,
+      next: NextFunction,
+    ) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+      return response.status(500).json({
         status: 'error',
-        message: error.message,
+        message: 'Internal server error',
       });
-    }
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  },
-);
+    },
+  );
 
-app.listen(8000, () => {
-  console.log('Server running on port 8000 😄');
-});
+  app.listen(8000, () => {
+    console.log('Server running on port 8000 😄');
+  });
+
+})
+
