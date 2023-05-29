56040a63331d13571d1f000d6f00125ae41509e1
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-05-29 20:47:26 -0300

    add user update avatar service

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/User';
+import UserRepository from '../typeorm/repositories/UserRepository';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+interface Request {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: Request): Promise<User> {
+    const user = await UserRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+    await UserRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
