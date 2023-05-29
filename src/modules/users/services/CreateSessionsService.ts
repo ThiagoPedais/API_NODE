78280cd5453a7c59d528359abae23819c4bb756d
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-05-29 19:49:34 -0300

    add jwt

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import User from '../typeorm/entities/User';
 import UserRepository from '../typeorm/repositories/UserRepository';
 import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface Request {
   email: string;
@@ -10,11 +11,11 @@ interface Request {
 
 interface Response {
   user: User
+  token: string
 }
 
 class CreateSessionsService {
-  public async execute({ email, password }: Request): Promise<User> {
-
+  public async execute({ email, password }: Request): Promise<Response> {
     const user = await UserRepository.findByEmail(email);
     if (!user) {
       throw new AppError('Password or email address invalid', 401);
@@ -25,7 +26,15 @@ class CreateSessionsService {
       throw new AppError('Password or email address invalid', 401);
     }
 
-    return user;
+    const token = sign({}, '1996b0e9031377d1f04360d205537611', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
