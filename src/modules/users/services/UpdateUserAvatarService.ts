aa3354fa1276ba8a1011c32d4bcd7df7cc4baa5a
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-06-01 15:38:13 -0300

    provider execute service upload

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import UserRepository from '../typeorm/repositories/UserRepository';
 import path from 'path';
 import uploadConfig from '@config/upload';
 import fs from 'fs';
+import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
 
 interface Request {
   user_id: string;
@@ -13,21 +14,26 @@ interface Request {
 class UpdateUserAvatarService {
   public async execute({ user_id, avatarFilename }: Request): Promise<User> {
     const user = await UserRepository.findById(user_id);
+    const storageProvider = new DiskStorageProvider();
 
     if (!user) {
       throw new AppError('User not found');
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      await storageProvider.deleteFile(user.avatar);
+      // const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      // const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      // if (userAvatarFileExists) {
+      //   await fs.promises.unlink(userAvatarFilePath);
+      // }
     }
+    const fileName = await storageProvider.saveFile(avatarFilename);
 
-    user.avatar = avatarFilename;
+    // user.avatar = avatarFilename;
+    user.avatar = fileName;
+
     await UserRepository.save(user);
 
     return user;
